4caf18b465066e6e15186d0083f6e00ede7b4046
Olivier Le Floch <olivier@le-floch.fr>
2009-03-17 15:53:37 +0000

    Version 0.1 de PyMorphogenesis   - Structure de base (à revoir),   - Utilisation de optparse,   - Utilisation de numpy,   - Début d'une réimplémentation d'un algorithme de résolution d'équation de diffusion.
    TODO :
      - Intégrer les effets de couplage entre les deux équations de diffusion,
      - Ajouter une interface de visualisation triviale (et lente) pour se convaincre que ça marche.

---
+++ PyMorphogenesis.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+PyMorphogenesis.py
+
+Created by Olivier Le Floch on 2009-03-17.
+Program written by Thomas Deniau and Olivier Le Floch.
+Copyright (c) 2009. All rights reserved.
+"""
+
+import sys
+import Image
+import math
+# Documentation for PIL is available at :
+# <http://www.pythonware.com/library/pil/handbook/index.htm>
+from PIL import Image, ImageFilter, ImageDraw, ImageChops, ImageStat, ImageMath
+from optparse import OptionParser
+
+program = 'PyMorphogenesis'
+version = 'Version 0.1, written by Thomas Deniau and Olivier Le Floch (c) 2009'
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv
+  
+  parser = OptionParser()
+  parser.add_option(
+    '-V', '--version', dest='version', default=False,
+    action="store_true",
+    help="show version information and exit", metavar='FILE')
+  
+  parser.add_option(
+    '-s', dest='D_s', type="float", default=0.04,
+    help="reaction rate parameter [default: %default]", metavar='RATE')
+  parser.add_option(
+    '-a', dest='D_a', type="float", default=0.25,
+    help="diffusion rate parameter for a [default: %default]", metavar='RATE')
+  parser.add_option(
+    '-b', dest='D_b', type="float", default=0.0625,
+    help="diffusion rate parameter for b [default: %default]", metavar='RATE')
+  parser.add_option(
+    '-d', dest='beta_i', type="float", default=12,
+    help="decay rate for b [default: %default]", metavar='DECAY')
+  
+  parser.add_option(
+    '-x', '--width', dest='width', type="int", default=20,
+    help="width of the generated texture [default: %default]", metavar='WIDTH')
+  parser.add_option(
+    '-y', '--height', dest='height', type="int", default=20,
+    help="height of the generated texture [default: %default]", metavar='HEIGHT')
+  
+  (options, args) = parser.parse_args()
+  
+  if options.version:
+    print program + ', ' + version
+    quit()
+  
+  print 'Generating texture with the following parameters :'
+  print '   Reaction rate =', options.D_s
+  print 'A diffusion rate =', options.D_a
+  print 'B diffusion rate =', options.D_b
+  print '    B decay rate =', options.beta_i
+  print ''
+  print '   texture width =', options.width
+  print '  texture height =', options.height
+  print '...'
+  
+  
+  
+  print 'Done !'
+
+# def imageMax(im):
+#   return max(im.getdata())
+# 
+# def imageSum(im):
+#   return sum(im.getdata())
+
+if __name__ == "__main__":
+  import psyco
+  psyco.full()
+  
+  sys.exit(main())
---
+++ Texture.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Texture.py
+
+Created by Olivier Le Floch on 2009-03-17.
+Program written by Thomas Deniau and Olivier Le Floch.
+Copyright (c) 2009. All rights reserved.
+"""
+
+import unittest
+from numpy import zeros, dot
+
+class Texture:
+  def __init__(self, width, height):
+    self.width  = width
+    self.height = height
+    
+    self.dx2     = 1.0 / width**2
+    self.dy2     = 1.0 / height**2
+    self.dnr_inv = 0.5 / (self.dx2 + self.dy2)
+    
+    self.grid_a = zeros((width, height), 'd')
+    self.grid_b = zeros((width, height), 'd')
+  
+  def step():
+    dx2     = self.dx2
+    dy2     = self.dy2
+    dnr_inv = self.dnr_inv
+    
+    g_a = self.grid_a
+    g_b = self.grid_b
+    
+    g_a[1:-1, 1:-1] =
+      ((g_a[0:-2, 1:-1] + g_a[2:, 1:-1]) * dy2
+        + (g_a[1:-1,0:-2] + g_a[1:-1, 2:]) * dx2) * dnr_inv
+    
+    g_b[1:-1, 1:-1] =
+      ((g_b[0:-2, 1:-1] + g_b[2:, 1:-1]) * dy2
+        + (g_b[1:-1,0:-2] + g_b[1:-1, 2:]) * dx2) * dnr_inv
+
+class TextureTests(unittest.TestCase):
+  def setUp(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
